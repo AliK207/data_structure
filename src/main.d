629914b747a686df22fdbe1b08dbc1b5a2b65171
ali <ali_khalaf1@hotmail.com>
2022-04-18 12:29:33 +0200

    .d

--- src/main.d
+++
@@ -1,2 +0,0 @@
-src/main.o: src/main.c src/../include/data.h src/../include/load_data.h \
- src/../include/requetes.h
--- src/requetes.d
+++
@@ -1,2 +0,0 @@
-src/requetes.o: src/requetes.c src/../include/data.h \
- src/../include/load_data.h src/../include/requetes.h
